e27d0311641129af9a1bc6a45787e5f59548e8d4
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-10-12 22:06:51 +0200

    Fixed ajax function, it needs refactor

--- web-layer/scripts/app.js
+++ web-layer/scripts/app.js
@@ -11,16 +11,17 @@
    * @description Retrieve all the info about developers
    */
   BazingaApp.getDevsInfo = function() {
-    var data = ajax('../data.json');
-    console.log(data);
+    ajax('http://localhost:3000/data.json', function(data) {
+      console.log(data);
+    });
   };
 
   /**
    * @description VanillaJS implementation for Ajax request
-   * @param {String} url        Url for the ajax request
-   * @return {Object} response  All the info from the request
+   * @param {String} url         Url for the ajax request
+   * @param {Function} callback  Callback function
    */
-  function ajax(url) {
+  function ajax(url, callback) {
     var response,
       httpRequest = new XMLHttpRequest();
 
@@ -29,22 +30,20 @@
     }
 
     // TBD
-    // httpRequest.onreadystatechange
-    try {
-      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-        if (httpRequest.status === 200) {
-          response = JSON.parse(httpRequest.responseText);
-          console.log(response);
+    httpRequest.onreadystatechange = function() {
+      try {
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+          if (httpRequest.status === 200) {
+            response = JSON.parse(httpRequest.responseText);
+            callback(response);
+          }
         }
+      } catch (err) {
+        throw new Error('There was a problem with the request.' + err);
       }
-    } catch (err) {
-      throw new Error('There was a problem with the request.' + err);
-    }
-
+    };
     httpRequest.open('GET', url);
     httpRequest.send();
-
-    return response;
   }
 
   if ( typeof(window.BazingaApp) === 'undefined') {
