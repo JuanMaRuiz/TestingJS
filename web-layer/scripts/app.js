9969f16d26c56ba3b0082783a164802938390de3
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-10-12 19:26:34 +0200

    Adding functionality for ajax request

--- web-layer/scripts/app.js
+++ web-layer/scripts/app.js
@@ -7,6 +7,30 @@
     return 'This is awesome!!';
   };
 
+  /**
+   * @description Retrieve all the info about developers
+   */
+  BazingaApp.getDevsInfo = function() {
+
+  };
+
+  /**
+   * @description VanillaJS implementation for Ajax request
+   * @param {String} url        Url for the ajax request
+   * @return {Object} response  All the info from the request
+   */
+  function ajax(url) {
+    var httpRequest = new XMLHttpRequest();
+
+    if( !httpRequest ) {
+      throw new Error('There was an error')
+    }
+
+    // TBD
+    // httpRequest.onreadystatechange
+
+  }
+
   if ( typeof(window.BazingaApp) === 'undefined') {
     window.BazingaApp = BazingaApp;
   }
