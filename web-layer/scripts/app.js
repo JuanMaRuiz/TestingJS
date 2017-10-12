4f2e6c8d06e56b0979426b0c8963b7b01457e2d9
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-10-12 20:25:30 +0200

    Added ajax method needs to be fixed

--- web-layer/scripts/app.js
+++ web-layer/scripts/app.js
@@ -11,7 +11,8 @@
    * @description Retrieve all the info about developers
    */
   BazingaApp.getDevsInfo = function() {
-
+    var data = ajax('../data.json');
+    console.log(data);
   };
 
   /**
@@ -20,15 +21,30 @@
    * @return {Object} response  All the info from the request
    */
   function ajax(url) {
-    var httpRequest = new XMLHttpRequest();
+    var response,
+      httpRequest = new XMLHttpRequest();
 
-    if( !httpRequest ) {
-      throw new Error('There was an error')
+    if ( !httpRequest ) {
+      throw new Error('There was an error');
     }
 
     // TBD
     // httpRequest.onreadystatechange
+    try {
+      if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+        if (httpRequest.status === 200) {
+          response = JSON.parse(httpRequest.responseText);
+          console.log(response);
+        }
+      }
+    } catch (err) {
+      throw new Error('There was a problem with the request.' + err);
+    }
 
+    httpRequest.open('GET', url);
+    httpRequest.send();
+
+    return response;
   }
 
   if ( typeof(window.BazingaApp) === 'undefined') {
