e7ade35cc616a7aa0449663cdc44802f3d58f6bf
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-10-14 18:13:44 +0200

    Added Ajax method (very basic) to jqLite library

--- web-layer/scripts/app.js
+++ web-layer/scripts/app.js
@@ -1,53 +1,24 @@
-(function() {
+(function(jqLite) {
   'use strict';
 
   var BazingaApp = BazingaApp || {};
 
-  BazingaApp.sayHello = function() {
-    return 'This is awesome!!';
+  BazingaApp.sayThanks = function() {
+    jqLite.thanks();
   };
 
   /**
-   * @description Retrieve all the info about developers
+   * Retrieve all the info about developers
    */
   BazingaApp.getDevsInfo = function() {
-    ajax('data.json', function(data) {
+    jqLite.ajax('data.json', function(data) {
       console.log(data);
     });
   };
-
-  /**
-   * @description VanillaJS implementation for Ajax request
-   * @param {String} url - Url for the ajax request
-   * @param {Function} callback - Callback function
-   */
-  function ajax(url, callback) {
-    var response,
-      httpRequest = new XMLHttpRequest();
-
-    if ( !httpRequest ) {
-      throw new Error('There was an error');
-    }
-
-    httpRequest.onreadystatechange = function() {
-      try {
-        if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-          if (httpRequest.status === 200) {
-            response = JSON.parse(httpRequest.responseText);
-            callback(response);
-          }
-        }
-      } catch (err) {
-        throw new Error('There was a problem with the request.' + err);
-      }
-    };
-    httpRequest.open('GET', url);
-    httpRequest.send();
-  }
 
   if ( typeof(window.BazingaApp) === 'undefined') {
     window.BazingaApp = BazingaApp;
   }
 
   return BazingaApp;
-})();
+})(jqLite);
---
+++ web-layer/scripts/jqLite.js
@@ -0,0 +1,52 @@
+/**
+ * jqLite v0.1.0
+ * This jqLite provides a simple API like jQuery to manipulate the DOM and add some basics useful methods like ajax request.
+ * Is build in vanillaJS and the browser supports is the main objective.
+ * @param {Object} window    - Window object is passed as parameter to avoid override it
+ * @param {Object} document  - Document object is passed as parameter to avoid override it
+ */
+(function(window) {
+  'use strict';
+
+  var VERSION = '0.1.0',
+    scope = window,
+    jqLite = {};
+
+  jqLite.thanks = function() {
+    console.log('Bazinga! Thanks for using jqLite version ' + VERSION);
+  };
+
+  /**
+   * VanillaJS implementation for Ajax request
+   * @param {String} url - Url for the ajax request
+   * @param {Function} callback - Callback function to execute when ajax response is received
+   */
+  jqLite.ajax = function(url, callback) {
+    var httpRequest = new XMLHttpRequest();
+
+    if ( !httpRequest ) {
+      throw new Error('Sorry there was an error creating an XMLHTTP instance');
+    }
+
+    httpRequest.onreadystatechange = makeRequest;
+    httpRequest.open('GET', url);
+    httpRequest.send();
+
+    /**
+     * @description Checks if the response was received and OK then send the response of the url provided to the callback function
+     */
+    function makeRequest() {
+      try {
+        if ( httpRequest.readyState == 4 && httpRequest.status == 200 ) {
+          callback(JSON.parse(httpRequest.responseText));
+        }
+      } catch (err) {
+        throw new Error('Sorry, there was an error processing your request: ' + err);
+      }
+    }
+  };
+
+  if ( !scope.jqLite ) {
+    scope.jqLite = jqLite;
+  }
+})(window);
