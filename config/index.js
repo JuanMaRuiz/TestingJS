26b3a86069c1508921b5b240d62961bfea535f05
JuanMa Ruiz <ruizjuanma@gmail.com>
2017-12-22 17:48:33 +0100

    Fixed Chrome headless

--- config/index.js
+++ config/index.js
@@ -8,5 +8,5 @@ var configSource = process.env.NODE_ENV || 'development';
 console.log(`process.env.NODE_ENV ${process.env.NODE_ENV} or process.env.TRAVIS ${process.env.TRAVIS}`);
 // var a = require('./env/development');
 // console.log(a);
-// module.exports = require('./env/' + configSource);
-module.exports = require('./env/test.js');
+module.exports = require('./env/' + configSource);
+// module.exports = require('./env/test.js');
