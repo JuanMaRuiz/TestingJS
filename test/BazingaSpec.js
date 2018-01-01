b71b4f099ba36b24fc21e50665e4c43d4229af34
JuanMa Ruiz <ruizjuanma@gmail.com>
2018-01-01 19:48:18 +0100

    Removed some unnecessary tests, added new test to check if bazingaApp is an Object, added John Resig to list of developers

--- test/BazingaSpec.js
+++ test/BazingaSpec.js
@@ -1,27 +1,10 @@
-describe("Dummy test", function() {
-    it("bazingaApp should be defined on window load", function() {
+describe("Generic tests for 'bazingaApp' library. 'bazingaApp'", function() {
+    it("should be defined on window load", function() {
         expect(bazingaApp).toBeDefined();
-        // expect(typeofBazingaApp).toEqual(Object);
+    });
+
+    it("should be and object", function() {
+        console.log(bazingaApp);
+        expect(typeof bazingaApp).toBe('object');
     });
 });
-
-// describe("Player", function () {
-//     var player;
-//     var song;
-
-//     // beforeEach(function () {
-//     //     player = new Player();
-//     //     song = new Song();
-//     // });
-
-//     it("should be able to play a Song", function () {
-//         player.play(song);
-//         expect(player.currentlyPlayingSong).toEqual(song);
-
-//         //demonstrates use of custom matcher
-//         expect(player).toBePlaying(song);
-//     });
-//     it("should be loaded as window property", function() {
-//         expect(BazingaApp.)toBeDefined();
-//     });
-// })
