8975525776331313246e62cf5d3b0e83ddced9a2
Juan Manuel Ruiz <juanmanuelruiz@Juans-MacBook-Pro.local>
2018-03-17 21:23:10 +0100

    Updated comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
 
   // Measures the time each task takes
   require('time-grunt')(grunt);
-  // Load grunt configuration based on grunt/aliases.yaml and execute all task under grunt folder
+  /**
+   * Load grunt configuration based on grunt/aliases.yaml and execute all tasks defined en every alias.
+   * Task are stored in grunt folder
+   */
   require('load-grunt-config')(grunt);
 };
