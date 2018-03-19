5c5fe8609c25951114a4d1b0f8b7c5033a2c224e
JuanMa Ruiz <ruizjuanma@gmail.com>
2018-03-19 16:31:59 +0100

    Added new tests for jqLite library.

--- test-examples/jasmine-karma/karma-config/env/development.js
+++ test-examples/jasmine-karma/karma-config/env/development.js
@@ -3,7 +3,9 @@
 module.exports = {
     karma: {
         singleRun: false,
+        failFast: true,
         browsers: ['Chrome'],
         reporters: ['progress', 'spec'],
+        plugins: ['karma-spec-reporter']
     },
 };
--- test-examples/jasmine-karma/karma-config/env/test.js
+++ test-examples/jasmine-karma/karma-config/env/test.js
@@ -3,7 +3,9 @@
 module.exports = {
     karma: {
         singleRun: true,
+        failFast: true,
         reporters: ['progress', 'spec','coverage'],
         browsers: ['ChromeHeadless'],
+        plugins: ['karma-spec-reporter']
     }
 }
