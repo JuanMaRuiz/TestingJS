d4530aab3521c7cf6a6d991ef31d4073ac532e47
juanmaruiz <ruizjuanma@gmail.com>
2018-03-22 21:33:16 +0100

    Fixed path for resources in mocha TestRunner.html

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const bs = require('browser-sync').create();
 bs.init({
     server: {
         baseDir: __dirname,
-        index: '/test-examples/jasmine-standalone-3.1.0/TestRunner.html',
+        index: '/test-examples/mocha/TestRunner.html',
     },
     host: 'localhost',
-    port: 3000  
+    port: 3000
 });
