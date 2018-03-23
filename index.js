7f85ca11622ea0e08e0fff670630b7c475a46606
JuanMa Ruiz <ruizjuanma@gmail.com>
2018-03-23 14:15:13 +0100

    Fixed server setting to reload on changes, added fixture examples

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const bs = require('browser-sync').create();
 
+// Listen to change events on HTML and reload
+bs.watch("test-examples/**/*.js").on("change", bs.reload);
+
 bs.init({
     server: {
         baseDir: __dirname,
-        index: '/test-examples/mocha/TestRunner.html',
+        index: '/test-examples/jasmine-standalone-3.1.0/TestRunner.html',
     },
     host: 'localhost',
     port: 3000
--- test-examples/jasmine-standalone-3.1.0/spec/jqLiteSpec.js
+++ test-examples/jasmine-standalone-3.1.0/spec/jqLiteSpec.js
@@ -1,10 +1,15 @@
+// Define generic path for fixtures
+jasmine.getFixtures().fixturesPath = '/test-examples/jasmine-standalone-3.1.0/spec/javascripts/fixtures/';
+
 describe("jqLite generic Tests", function () {
   it("jqLite should be defined in window object", function () {
     expect(window.jqLite).toBeDefined();
   });
-  describe("jqLite methods", function () {
-    it('jqLite.qsa should return and array', function () {
-      expect(jqLite.qsa('div')).toEqual([]);
+
+  describe("jqLite.qs() method", function () {
+    it('should find an element in the DOM', function () {
+      loadFixtures('fragment.html');
+      expect(jqLite.qs('.bazinga')).toExist();
     });
   });
 });
