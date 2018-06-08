dd0879978bd37ec653905e987c4dcf39c3ede45e
juanmaruiz <ruizjuanma@gmail.com>
2018-06-08 22:53:07 +0200

    Moving templates to es6 template literal

--- app/scripts/developer.js
+++ app/scripts/developer.js
@@ -21,4 +21,22 @@ class Developer { // eslint-disable-line no-unused-vars
     this.twitter = twitter;
     this.github = github;
   }
+
+  /**
+   * Generates the template for a developer with the given data. Please, note that this is not the proper way to do. You should use a template
+   * engine to generate the html instead of generater is this way.
+   * @return {String} Contains the html to render
+   */
+  render() {
+    return `<div class="card border-light mb-3">
+    <div class="card-header">${this.name}</div>
+    <div class="card-body">
+    <p class="img"><img id="dev-image" class="img-rounded" src="${this.avatar}" alt="${this.name}" style="height: 200px"></p>
+        <p class="card-text">${this.bio}</p>
+        <p class="card-text"><span class="glyphicon glyphicon-link" aria-hidden="true"></span> <a href="${this.web}">${this.web}</a></p>
+        <p class="card-text"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <a href="${this.twitter}">${this.twitter}</a></p>
+        <p class="card-text"><span class="glyphicon glyphicon-cloud" aria-hidden="true"></span> <a href="${this.github}">${this.github}</a></p>
+      <div>
+    </div>`;
+  }
 }
--- app/scripts/developerTemplate.js
+++ app/scripts/developerTemplate.js
@@ -1,42 +1,30 @@
 (function(scope) {
   'use strict';
-
-
   /**
    * Define the default template for the DeveloperTemplate panel
    * @constructor
    */
   function DeveloperTemplate() {
-    this.defaultTemplate = '<div class="card border-light mb-3">' +
-    '<div class="card-header">{{dev.name}}</div>' +
-    '<div class="card-body">'+
-    '<p class="img"><img id="dev-image" class="img-rounded" src="{{dev.image}}" alt="{{dev.name}}" style="height: 200px"></p>' +
-        '<p class="card-text">{{dev.bio}}</p>' +
-        '<p class="card-text"><span class="glyphicon glyphicon-link" aria-hidden="true"></span> <a href="{{dev.web}}">{{dev.web}}</a></p>' +
-        '<p class="card-text"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <a href="{{dev.twitter}}">{{dev.twitter}}</a></p>' +
-        '<p class="card-text"><span class="glyphicon glyphicon-cloud" aria-hidden="true"></span> <a href="{{dev.github}}">{{dev.github}}</a></p>' +
-      '<div>' +
-    '</div>';
+    // TODO this constructor should be removed and the method too
   }
 
   /**
    * Generates the template for a developer with the given data. Please, note that this is not the proper way to do. You should use a template
    * engine to generate the html instead of generater is this way.
-   * @param {Object} data
+   * @param {Object} dev
    * @return {String} Contains the html to render
    */
-  DeveloperTemplate.prototype.render = function(data) {
-    let template = this.defaultTemplate,
-      view = '';
-
-    template = template.replace(/{{dev.image}}/g, data.avatar);
-    template = template.replace(/{{dev.name}}/g, data.name);
-    template = template.replace(/{{dev.bio}}/g, data.bio);
-    template = template.replace(/{{dev.web}}/g, data.web);
-    template = template.replace(/{{dev.twitter}}/g, data.twitter);
-    template = template.replace(/{{dev.github}}/g, data.github);
-    view += template;
-    return view;
+  DeveloperTemplate.prototype.render = function(dev) {
+    return `<div class="card border-light mb-3">
+    <div class="card-header">${dev.name}</div>
+    <div class="card-body">
+    <p class="img"><img id="dev-image" class="img-rounded" src="${dev.avatar}" alt="${dev.name}" style="height: 200px"></p>
+        <p class="card-text">${dev.bio}</p>
+        <p class="card-text"><span class="glyphicon glyphicon-link" aria-hidden="true"></span> <a href="${dev.web}">${dev.web}</a></p>
+        <p class="card-text"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <a href="${dev.twitter}">${dev.twitter}</a></p>
+        <p class="card-text"><span class="glyphicon glyphicon-cloud" aria-hidden="true"></span> <a href="${dev.github}">${dev.github}</a></p>
+      <div>
+    </div>`;
   };
 
   scope.bazingaApp = scope.bazingaApp || {};
