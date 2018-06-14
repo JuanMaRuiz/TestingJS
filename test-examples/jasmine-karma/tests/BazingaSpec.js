bb903a8c07eb6634248a11dc27c468b3848f6b7c
juanmaruiz <ruizjuanma@gmail.com>
2018-06-14 19:55:16 +0200

    Removed .nyc reports added documentation for jasmine and mocha server and removed unnecesary test into BazingaSpec

--- test-examples/jasmine-karma/tests/BazingaSpec.js
+++ test-examples/jasmine-karma/tests/BazingaSpec.js
@@ -1,8 +1,4 @@
 describe("Generic tests for 'bazingaApp'", function() {
-    it("should be defined on window load", function() {
-        expect(bazingaApp).toBeDefined();
-    });
-
     it("should be and object", function() {
         expect(typeof bazingaApp).toBe('object');
     });
