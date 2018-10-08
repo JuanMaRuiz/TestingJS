3d41030aa54beaff6e68b299dd614ff82d433d8f
JuanMa Ruiz <ruizjuanma@gmail.com>
2018-10-09 01:03:22 +0200

    Added mocha test for developerTemplate

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const bs = require('browser-sync').create();
 
 // Listen to change events on HTML and reload
-bs.watch("test-examples/**/*.js").on("change", bs.reload);
+bs.watch('test-examples/**/*.js').on('change', bs.reload);
 
 bs.init({
-    server: {
-        baseDir: __dirname,
-        index: `/test-examples/${process.argv[2]}/TestRunner.html`,
-    },
-    host: 'localhost',
-    port: 3000
+  server: {
+    baseDir: __dirname,
+    index: `/test-examples/${process.argv[2]}/TestRunner.html`,
+  },
+  host: 'localhost',
+  port: 3000,
 });
