d1f757b1a513e75566092ee6d8f4c0d0dad0e911
JuanMa Ruiz <ruizjuanma@gmail.com>
2019-12-29 22:15:49 +0100

    Using bdd

--- test-examples/mocha/test/jqLite.test.js
+++ test-examples/mocha/test/jqLite.test.js
@@ -1,25 +1,26 @@
 const log = console.log;
 
-suite("Generic Tests for jqLite Library", function() {
-    test("should be defined on window load", function() {
+suite("Generic Tests for jqLite Library", () => {
+    test("should be defined on window load", () => {
         assert.isDefined(window.jqLite, 'jqLite has been defined')
     });
 
-  beforeEach(function () {
+  beforeEach(() => {
     // add an element to the page to render into
     let testElement = $('<section></section>');
     $('body').append(testElement);
   });
-  test('jqLite.qsa should return an array', function() {
+  test('jqLite.qsa should return an array', () => {
+    expect(jqLite.qsa('section')).to.be.an('array');
     assert.typeOf(jqLite.qsa('section'), "array", "we have an array");
   });
 
-  before(function () {
+  before(() => {
     // add an element to the page to render into
     let testElement = $('<div class="foo"></div class="foo"><div class="foo"></div class="foo">');
     $('body').append(testElement);
   });
-  test('jqLite.qsa should find 2 foo elements in the DOM', function () {
+  test('jqLite.qsa should find 2 foo elements in the DOM', () => {
     assert.lengthOf(jqLite.qsa('.foo'), 2, "2 div.foo elements were found in html");
   });
 });
